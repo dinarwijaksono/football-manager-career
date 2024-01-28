f84732721cf478b1c51abec6b013c2ec28395e52
dinarwijaksono11 <dinarwijaksono11@gmail.com>
2024-01-28 23:45:30 +0700

    Desain: setup halaman Home/index

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FMC</title>
+
+    <link rel="stylesheet" href="/asset/tailwind/style.css">
+
+</head>
+
+<body>
+
+    <main class="border w-[500px] absolute top-12 right-20 bg-slate-100 shadow-md">
+
+        <section class="p-2">
+            <h1 class="mb-4 font-bold text-center">Football Manager Career</h1>
+
+            <ul>
+                <a href="/Home/new-profile">
+                    <li class="bg-green-600 p-2 rounded mb-2 text-white hover:underline hover:bg-green-500">Buat profile
+                        baru
+                    </li>
+                </a>
+
+                <a href="/Home/load-profile">
+                    <li class="bg-green-600 p-2 rounded mb-2 text-white hover:underline hover:bg-green-500">Muat profile
+                    </li>
+                </a>
+            </ul>
+        </section>
+
+    </main>
+
+</body>
+
+</html>
